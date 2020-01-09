554cb8a7dfb2e42b4cf3917c5d6dc21e508ffe0b
alexa <alexa.griffin42@gmail.com>
2020-01-09 10:42:41 -0500

    better mime

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express"
 import sockjsNode from "sockjs"
 import jsxs from "jsx-static"
 import path from "path"
-import MemoryFileSystem from "memory-fs"
+import mfs, {vol} from "memfs"
 import http from "http"
 import open from "open"
 //@ts-ignore // for some reason this isn't happy
@@ -13,24 +13,25 @@ import portFinder from "portfinder"
 const sockInjection = `
 <script src="sockjs.min.js"></script>
 <script>
-(function() {
-  var sockjs_url = '/echo'
-  var sockjs = new SockJS(sockjs_url)
-  
-  sockjs.onopen = function() { console.log('opened socket', sockjs.protocol) }
-  sockjs.onmessage = function(e) {
-    if(JSON.parse(e.data).msg === "reload") location.reload()
-  }
-  sockjs.onclose = function() { console.log('socket disconnected') }
-})()
+  (function() {
+    var sockjs_url = '/echo'
+    var sockjs = new SockJS(sockjs_url)
+    
+    sockjs.onopen = function() { console.log('opened socket', sockjs.protocol) }
+    sockjs.onmessage = function(e) {
+      if(JSON.parse(e.data).msg === "reload") location.reload()
+    }
+    sockjs.onclose = function() { console.log('socket disconnected') }
+  })()
 </script>
 `
 
 export function run(config: JsxsConfig) {
-  const serverFs = new MemoryFileSystem()
+  // const serverFs = new MemoryFileSystem()
 
   jsxs.watch({
-    outputFs: serverFs,
+    ...config,
+    outputFs: mfs,
     outputDir: "/",
     hooks: {
       postProcess: src => src + sockInjection,
@@ -54,13 +55,16 @@ export function run(config: JsxsConfig) {
 
   const server = express()
   server.use(express.static(path.join(path.dirname(require.resolve("sockjs-client")), "..", "dist")))
+  server.use(express.static("/assets"))
   
-  server.get("/*", (req, res) => {
-    res.setHeader("Content-Type", "text/html")
-    if(serverFs.existsSync(req.path)) res.send(serverFs.readFileSync(req.path))
-    else if(serverFs.existsSync(req.path + ".html")) res.send(serverFs.readFileSync(req.path + ".html"))
-    else if(serverFs.existsSync(req.path + "index.html")) res.send(serverFs.readFileSync(req.path + "index.html"))
-    else if(serverFs.existsSync("/404.html")) res.send(serverFs.readFileSync("404.html"))
+  server.get("*", (req, res) => {
+    // res.setHeader("Content-Type", "text/html")
+    res.type(path.extname(req.path) || "html")
+
+    if(mfs.existsSync(req.path) && mfs.statSync(req.path).isFile()) res.send(mfs.readFileSync(req.path))
+    else if(mfs.existsSync(req.path + ".html") && mfs.statSync(req.path + ".html").isFile()) res.send(mfs.readFileSync(req.path + ".html"))
+    else if(mfs.existsSync(req.path + "index.html") && mfs.statSync(req.path + "index.html").isFile()) res.send(mfs.readFileSync(req.path + "index.html"))
+    else if(mfs.existsSync("/404.html") && mfs.statSync("/404.html").isFile()) res.send(mfs.readFileSync("404.html"))
     else res.send("nice happy 404") //TODO: create a nice default 404 page
   })
   
